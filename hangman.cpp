222eec2835ea2032116320612a2e26e89186ba39
Owen Larson <owenslar@byu.edu>
2024-11-13 16:03:52 -0700

    words text file

---
+++ hangman.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to Hangman!" << endl;
+    
+
+    return 0;
+}
