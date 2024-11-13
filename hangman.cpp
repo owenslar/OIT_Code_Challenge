4d14b5f6b4ad2179a56cf6cdbf2425ce89e49778
Owen Larson <owenslar@byu.edu>
2024-11-13 16:58:18 -0700

    can now create solution vector and guess vector

--- hangman.cpp
+++ hangman.cpp
@@ -11,13 +11,31 @@
 
 using namespace std;
 
-// temporary function to help test
+// function that prints out values in a vector
 void print_vector(vector<string> v) {
     for (int i = 0; i < v.size(); i++) {
-        cout << v[i] << endl;
+        cout << v[i];
     }
+    cout << endl;
 }
 
+// function that creates a vector filled with the specified item
+void create_filled_vector(vector<string> &filled_vector, string fill_item, int vector_size) {
+    for (int i = 0; i < vector_size; i++) {
+        filled_vector.push_back(fill_item);
+        filled_vector.push_back(" ");
+    }
+    return;
+}
+
+// function that creates a vector with the letters to the solution word in it
+void create_solution_vector(vector<string> &solution_vector, string solution) {
+    for (int i = 0; i < solution.size(); i++) {
+        string letter = string(1, solution[i]);
+        solution_vector.push_back(letter);
+    }
+    return;
+}
 
 int main() {
     cout << "Welcome to Hangman!" << endl;
@@ -35,7 +53,29 @@ int main() {
     srand(static_cast<unsigned>(time(nullptr)));
     string solution_word = words_vector[rand() % words_vector.size()];
 
-    cout << "Word: " << solution_word << endl;
+    cout << solution_word << endl;
+
+    cout << "The word has " << solution_word.size() << " letters" << endl;
+    
+    // create solution vector, blank vector, and guess vector
+    vector<string> guess_vector;
+    vector<string> blank_vector;
+    vector<string> solution_vector;
+    create_filled_vector(guess_vector, "", solution_word.size());
+    create_filled_vector(blank_vector, "_", solution_word.size());
+    create_solution_vector(solution_vector, solution_word);
+
+    // main loop that perpetuates the game
+    // while (true) {
+    //     string guess;
+    //     cout << "Guess a letter: ";
+    //     getline(cin, guess);
+    //     // in case user enters nothing, or 2 or more letters
+    //     if (guess.size() > 1 || guess.empty()) {
+    //         cout << "Please enter a singular letter" << endl;
+    //         continue;
+    //     }
+    // }
 
     return 0;
 }
