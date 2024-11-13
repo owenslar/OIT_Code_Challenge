f161ded7a7f333d2060d21d6b3d17013928d0531
Owen Larson <owenslar@byu.edu>
2024-11-13 16:18:03 -0700

    read words into vector working

--- hangman.cpp
+++ hangman.cpp
@@ -7,9 +7,25 @@
 
 using namespace std;
 
+void print_vector(vector<string> v) {
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << endl;
+    }
+}
 int main() {
     cout << "Welcome to Hangman!" << endl;
-    
+
+    // Grabbing words from words.txt and putting them in a vector
+    ifstream input("words.txt");
+    vector<string> words_vector;
+    while (!input.eof()) {
+        string word;
+        getline(input, word);
+        words_vector.push_back(word);
+    }
+    // cout << "vector: " << words_vector << endl;
+    print_vector(words_vector);
 
     return 0;
 }
+
